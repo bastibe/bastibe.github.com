8f1b32cda970094e638b8659cebbd3cddd2458bc
Bastian Bechtold <bastibe.dev@mailbox.org>
2025-12-13 16:16:43 +0100

    index.html added to bsbr

--- bsbr.py
+++ bsbr.py
@@ -1,6 +1,7 @@
 import pathlib
 import time
 import tomllib
+import collections
 
 import mistune
 
@@ -16,7 +17,8 @@ def main():
     markdown_parser = mistune.create_markdown()
 
     # Iterate through all files in the drafts directory
-    posts = {}
+    Post = collections.namedtuple('Post', ['path', 'metadata', 'body'])
+    posts = []
     for file in posts_path.glob('*.md'):
         markdown = file.read_text(encoding='utf-8')
         front_matter, markdown = split_frontmatter(markdown)
@@ -26,13 +28,18 @@ def main():
         html = assemble_post(config, metadata, page_url, body, base_url)
         output_path = root_path / f"{file.stem}.html"
         output_path.write_text(html, encoding='utf-8')
-        posts[output_path] = metadata
+        posts.append(Post(output_path, metadata, body))
+    posts = sorted(posts, key=lambda post: post.metadata['date'], reverse=True)
     
     # create archive.html page
-    html = assemble_archive(config, posts, base_url, root_path)
+    html = assemble_archive(config, posts, base_url)
     archive_path = root_path / 'archive.html'
     archive_path.write_text(html, encoding='utf-8')
     
+    # create index.html page
+    html = assemble_index(config, posts, base_url, num_posts=config['index_num_posts'])
+    index_path = root_path / 'index.html'
+    index_path.write_text(html, encoding='utf-8')
 
 def split_frontmatter(text):
     front_matter_start = text.find('---')
@@ -74,7 +81,7 @@ def assemble_post(config, metadata, page_url, body, base_url, is_index=False):
 {config['page_header']}
 <div id="content">
 {f'<div class="post-date">{date}</div>' if not is_index else ''}
-<h1 class="post-title"><a href="{page_url}">{metadata['title']}</a></h1>
+{f'<h1 class="post-title"><a href="{page_url}">{metadata["title"]}</a></h1>' if not is_index else ''}
 {body}
 <div class="taglist"><a href="{base_url}/tags.html">Tags</a>: {taglist}</div>
 {f'<div id="comments">\n{config["page_comments"]}\n</div>' if not is_index else ''}
@@ -87,10 +94,9 @@ def assemble_post(config, metadata, page_url, body, base_url, is_index=False):
 """
 
 
-def assemble_archive(config, posts, base_url, root_path):
-    posts = dict(sorted(posts.items(), key=lambda item: item[1]['date'], reverse=True))
-    content = []
-    for post_path, metadata in posts.items():
+def assemble_archive(config, posts, base_url):
+    content = ['<h1>Archive</h1>']
+    for post_path, metadata, body in posts:
         post_url = f"{base_url}/posts/{post_path.stem}.html"
         date = time.strftime(config['date_format'], time.strptime(metadata['date'], '%Y-%m-%d'))
         content.append(f'<div class="post-date">{date}</div><h2 class="post-title"><a href="{post_url}">{metadata["title"]}</a></h2>')
@@ -98,6 +104,21 @@ def assemble_archive(config, posts, base_url, root_path):
 
     return assemble_post(config, {'title': 'Archive', 'date': time.strftime('%Y-%m-%d'), 'filetags': ''}, 
                          f"{base_url}/archive.html", html, base_url, is_index=True)
+
+
+def assemble_index(config, posts, base_url, num_posts=5):
+    content = []
+    for post_path, metadata, body in posts[:num_posts]:
+        post_url = f"{base_url}/posts/{post_path.stem}.html"
+        date = time.strftime(config['date_format'], time.strptime(metadata['date'], '%Y-%m-%d'))
+        content.append(f"""
+<div class="post-date">{date}</div><h2 class="post-title"><a href="{post_url}">{metadata["title"]}</a></h2>
+{body}
+""")
+    html = '\n'.join(content)
+
+    return assemble_post(config, {'title': 'Home', 'date': time.strftime('%Y-%m-%d'), 'filetags': ''}, 
+                         f"{base_url}/index.html", html, base_url, is_index=True)
 
 
 if __name__ == "__main__":
